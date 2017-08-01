75b2b4726c55a39dc8446d21601caa69e25d8fe5
Viktor Verebelyi <vviktor2@gmail.com>
2017-08-01 22:20:36 +0100

    Remove unnecessary ${ROOTFS_DIR}

--- stage4/01-install-qtgstreamer/00-run-chroot.sh
+++ stage4/01-install-qtgstreamer/00-run-chroot.sh
@@ -18,7 +18,7 @@ if [ ! -d /opt/qt-gstreamer ] || [ ! -z ${BUILD_QTGSTREAMER+x} ]; then
     cd build
 
     #cmake QtGstreamer
-    cmake .. -DCMAKE_INSTALL_PREFIX=${ROOTFS_DIR}/usr -DCMAKE_INSTALL_LIBDIR=lib/$(dpkg-architecture -qDEB_HOST_MULTIARCH) -DCMAKE_INSTALL_INCLUDEDIR=include -DQT_VERSION=5 -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-std=c++11cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=lib/$(dpkg-architecture -qDEB_HOST_MULTIARCH) -DCMAKE_INSTALL_INCLUDEDIR=include -DQT_VERSION=5 -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-std=c++11
+    cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=lib/$(dpkg-architecture -qDEB_HOST_MULTIARCH) -DCMAKE_INSTALL_INCLUDEDIR=include -DQT_VERSION=5 -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-std=c++11cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=lib/$(dpkg-architecture -qDEB_HOST_MULTIARCH) -DCMAKE_INSTALL_INCLUDEDIR=include -DQT_VERSION=5 -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-std=c++11
 
     #Make and install QtGstreamer
     make -j6
