37290e8dd2e5a5b441a6c96fe53cc0b602ba9fce
viktorgino <vviktor2@gmail.com>
2017-08-20 21:50:10 +0100

    [STAGE4] Add welle.io and qtcharts to build step

--- stage4/01-install-headunit/00-run.sh
+++ stage4/01-install-headunit/00-run.sh
@@ -24,10 +24,31 @@ on_chroot << EOF
 
     #Make and install QtGstreamer
     make -j6
-    sudo make install
+    make install
 EOF
 else
     log "Skipping qt-gstreamer compilation"
+fi
+
+#Compile qtcharts
+if [ ! -d ${ROOTFS_DIR}/opt/qtcharts ] || [ ! -z ${BUILD_QTCHARTS+x} ]; then    
+    log "Compiling qtcharts"
+on_chroot << EOF
+    cd /opt
+
+    #Clone qtcharts
+    git clone https://github.com/qt/qtcharts.git -b 5.7
+
+    cd qtcharts/src
+    git checkout tags/v5.7.1
+
+    #Make and install qtcharts
+    qmake CONFIG+=release
+    make -j4
+    make install
+EOF
+else
+    log "Skipping qtcharts compilation"
 fi
 
 #Compile and install headunit-desktop
@@ -39,8 +60,10 @@ on_chroot << EOF
     ### Build headunit 
     if [ -d "headunit-desktop" ]; then
         cd headunit-desktop
+        echo "Pulling headunit from git"
         git pull
     else
+        echo "Cloning headunit from git"
         git clone --recursive --depth 1 -j6 https://github.com/viktorgino/headunit-desktop.git
         cd headunit-desktop
     fi
@@ -50,8 +73,9 @@ on_chroot << EOF
 
     #compile headunit-desktop
     make clean
-    qmake -config release
+    qmake CONFIG+=welleio -config release 
     make -j4
+    chown -R pi:pi /opt/headunit-desktop
 EOF
 else
     log "Skipping headunit-desktop compilation"
